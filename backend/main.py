1ab772ef342a15672604c222c18ddc54884fe487
sw rhee <cupsarang@gmail.com>
2026-08-06 17:29:49 +0900

    CORS 부분 해결 - 모든 브라우져에서 사용가능함

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["*"],    
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     # allow_origins=["http://localhost:5173", "https://example.com"] 
     allow_credentials=True,
     allow_methods=["*"],
